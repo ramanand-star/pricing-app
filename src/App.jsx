d34638f3f6e747ddccd6c19699221e058193c7db
ramanand-star <72610379+ramanand-star@users.noreply.github.com>
2024-12-24 11:55:16 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import UploadFile from './UploadFile';
 import SearchRecords from './SearchRecords';
 import UpdateRecord from './UpdateRecord';
+import './App.css';
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Pricing Records Management</h1>
+//       <UploadFile />
+//       <SearchRecords />
+//       <UpdateRecord />
+//     </div>
+//   );
+// };
+
+function App() {
   return (
-    <div>
-      <h1>Pricing Records Management</h1>
-      <UploadFile />
-      <SearchRecords />
-      <UpdateRecord />
+    <div className="App">
+      <h1 className="title">Pricing Records Management</h1>
+      <p>This is a sample application.</p>
+         <UploadFile />
+         <SearchRecords />
+         <UpdateRecord />
     </div>
   );
 };
